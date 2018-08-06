3c84ce9ee9de289dc2d4225d1b3793dbf09a1b1c
Igor Mitin <igor.mitin@outlook.com>
2018-08-06 18:39:31 +0300

    Поправка еще одного косяка в интеграции с github

--- project_git_github/models/github.py
+++ project_git_github/models/github.py
@@ -84,7 +84,7 @@ class GitPayloadParser(models.AbstractModel):
     # Payload
     # -------------------------------------------
     def parse_github_payload(self, context):
-        method_name = self, "parse_github_%s" % context.action_type
+        method_name = "parse_github_%s" % context.action_type
         parse_event_method = getattr(self, method_name)
         return parse_event_method(context)
 
